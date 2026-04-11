f0a272f345a79f2672db704262a37029190c2914
Dimaskin100 <dimaskin100discord@gmail.com>
2026-04-11 13:41:38 +0300

    Update startup.lua

--- Programs/ForComputers/startup.lua
+++ Programs/ForComputers/startup.lua
@@ -1,6 +1,12 @@
+-- This program for ComputerCraft
+-- This program auto running programs
+-- In library MIF, set program to "startup" and write to file "program" name your program
+
 if fs.exists("mif.lua") == false then
 shell.run("wget https://raw.githubusercontent.com/Dimaskin100/Programs-for-computercraft/refs/heads/main/Programs/ForComputers/mif.lua")
 end
+
+term.clear()
 
 local mif = require("mif")
 
