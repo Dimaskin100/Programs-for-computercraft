b7de4fd53ab23c296edd35d21089b64ea922d052
Dimaskin100 <dimaskin100discord@gmail.com>
2026-05-02 19:15:59 +0300

    Update heh.lua

--- Programs/ForComputers/heh.lua
+++ Programs/ForComputers/heh.lua
@@ -1,16 +1,15 @@
 term.clear()
 
-local file = io.open(arg[2], "r")
+local file = io.open(arg[1], "r")
 
 if file then
   file:close()
 else
   print("Error")
-  file:close()
   while true do io.open() end
 end
 
-for line in io.lines(arg[2]) do
+for line in io.lines(arg[1]) do
   local text = ""
     for number in string.gmatch(line, "%S+") do
     if number == "0" then
